242a3e7d61f89c9389a0b31fbf1df2dd7e553592
Atikur Rahman <mratikurrahman@gmail.com>
2024-05-07 23:28:28 +0600

    Project has initialized

---
+++ iTraveler/iTravelerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTravelerApp.swift
+//  iTraveler
+//
+//  Created by Atikur Rahman on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct iTravelerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
