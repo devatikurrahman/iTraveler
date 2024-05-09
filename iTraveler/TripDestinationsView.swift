c37505b7875a6532e52ab8c56488605a7c4588fa
Atikur Rahman <mratikurrahman@gmail.com>
2024-05-10 02:36:05 +0600

    Added coordinate and marker on the map

--- iTraveler/TripDestinationsView.swift
+++ iTraveler/TripDestinationsView.swift
@@ -11,7 +11,22 @@ import MapKit
 struct TripDestinationsView: View {
     @State private var cameraPosition: MapCameraPosition = .automatic
     var body: some View {
-        Map()
+        Map(position: $cameraPosition) {
+            //40.752800, -73.977152
+            Marker(
+                "Grand Central Terminal",
+                coordinate: CLLocationCoordinate2D(
+                    latitude: 40.752800,
+                    longitude: -73.977152
+                )
+            )
+        }
+            .onAppear() {
+                let newYork = CLLocationCoordinate2D(latitude: 40.712982, longitude: -74.007205);
+                let span = MKCoordinateSpan(latitudeDelta: 0.15, longitudeDelta: 0.15)
+                let region = MKCoordinateRegion(center: newYork, span: span)
+                cameraPosition = .region(region)
+            }
     }
 }
 
